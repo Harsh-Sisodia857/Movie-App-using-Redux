386168613191d8cfe32924b3c978217dadd9abf2
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-05-04 19:14:04 +0530

    Add Middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore,applyMiddleware } from 'redux';
 import rootReducer from './reducers/index'
+
+// middleware function logger(obj,next,action). obj consist of dispatch and getState
+//redux internally called logger as logger(obj),(next)(action)
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code 
+      console.log("ACTION_TYPE = ", action.type);
+      next(action);
+    }
+  }
+}
+
 
 // ADDDING STORE TO APPLICATION
 //passing reducer to createStore
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(logger));
 console.log('store :',store.getState());  
 
 
